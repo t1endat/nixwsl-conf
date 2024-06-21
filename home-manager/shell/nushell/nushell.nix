a96933dd4223b59ebd7f7535fd1d79878c9eaea0
tiendat <tiendatlework@gmail.com>
2024-06-21 18:28:03 +0000

    nushell env.nu

--- home-manager/shell/nushell/nushell.nix
+++ home-manager/shell/nushell/nushell.nix
@@ -2,18 +2,19 @@
 let
   theme = builtins.readFile ./theme.nu;
   extraConfig = builtins.readFile ./configExtra.nu;
+  ROOT = builtins.toString ./.;
 in {
   programs.nushell = {
     enable = true;
     configFile.source =
       "${nushell-defaultConfig}/crates/nu-utils/src/sample_config/default_config.nu";
-    # envFile.source =
-    #   "${nushell-defaultConfig}/crates/nu-utils/src/sample_config/default_env.nu";
+    envFile.source =
+      "${ROOT}/env.nu";
     extraConfig = ''
       ${theme}
       ${extraConfig}
     '';
-    # extraEnv = builtins.readFile ./envExtra.nu;
+    extraEnv = builtins.readFile ./envExtra.nu;
   };
 }
 
